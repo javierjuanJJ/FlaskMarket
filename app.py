53175426d6b45f732b7c6250c1749efa5c8e759e
Javier Juan Uceda <javierjuanuceda18@gmail.com>
2023-03-06 11:18:44 +0100

    Commit 1 1.Introduction

--- app.py
+++ app.py
@@ -7,6 +7,10 @@ app = Flask(__name__)
 def hello_world():  # put application's code here
     return 'Hello World!'
 
+@app.route('/about/<username>')
+def about_page(username):
+    return f'<h1>This is the about page of {username}'
+
 
 if __name__ == '__main__':
     app.run()
