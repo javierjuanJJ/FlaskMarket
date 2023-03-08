027cc75db82572f42be41134e1c37e00537d8604
Javier Juan Uceda <javierjuanuceda18@gmail.com>
2023-03-08 11:42:46 +0100

    Commit 10 P10 Flashes & Advanced Validations

--- market/routes.py
+++ market/routes.py
@@ -1,5 +1,5 @@
 
-from flask import render_template, redirect, url_for
+from flask import render_template, redirect, url_for, flash
 
 from market import app, db
 from market.forms import RegisterForm
@@ -28,6 +28,6 @@ def register_page():
         return redirect(url_for('market_page'))
     if form.errors != {}: #If there are not errors from the validations
         for err_msg in form.errors.values():
-            print(f'There was an error with creating a user: {err_msg}')
+            flash(f'There was an error with creating a user: {err_msg}', category='danger')
 
     return render_template('register.html', form=form)
