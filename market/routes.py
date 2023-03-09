61064d381c97f67e551651259b76966d56d6dfb7
Javier Juan Uceda <javierjuanuceda18@gmail.com>
2023-03-09 10:10:00 +0100

    Commit 14 13 - Logout & Customizations

--- market/routes.py
+++ market/routes.py
@@ -4,7 +4,7 @@ from flask import render_template, redirect, url_for, flash
 from market import app, db
 from market.forms import RegisterForm, LoginForm
 from market.models import Item, User
-from flask_login import login_user
+from flask_login import login_user, logout_user
 
 
 @app.route('/')
@@ -50,3 +50,10 @@ def login_page():
             flash('Username and password are not match! Please try again', category='danger')
 
     return render_template('login.html', form=form)
+
+
+@app.route('/logout')
+def logout_page():
+    logout_user()
+    flash("You have been logged out!", category='info')
+    return redirect(url_for("home_page"))
