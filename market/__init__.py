34a693418a13322e5cad3e47d84d6587b94f2a1d
Javier Juan Uceda <javierjuanuceda18@gmail.com>
2023-03-09 10:04:45 +0100

    Commit 13 12 - User Authentication Part 2

--- market/__init__.py
+++ market/__init__.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 
@@ -7,6 +8,6 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market.db'
 db = SQLAlchemy(app)
 app.config['SECRET_KEY'] = 'ec9439cfc6c796ae2029594d'
 bcrypt = Bcrypt(app)
-
+login_manager = LoginManager(app)
 
 from market import routes
--- market/routes.py
+++ market/routes.py
@@ -4,6 +4,7 @@ from flask import render_template, redirect, url_for, flash
 from market import app, db
 from market.forms import RegisterForm, LoginForm
 from market.models import Item, User
+from flask_login import login_user
 
 
 @app.route('/')
@@ -23,6 +24,7 @@ def register_page():
         user_to_create = User(username=form.username.data,
                               email_address=form.email_address.data,
                               password_hash=form.password1.data)
+        user_to_create.password = form.password1.data
         db.session.add(user_to_create)
         db.session.commit()
         return redirect(url_for('market_page'))
@@ -36,4 +38,15 @@ def register_page():
 @app.route('/login', methods=['GET', 'POST'])
 def login_page():
     form = LoginForm()
+    if form.validate_on_submit():
+        attempted_user = User.query.filter_by(username=form.username.data).first()
+        if attempted_user and attempted_user.check_password_correction(
+                attempted_password=form.password.data
+        ):
+            login_user(attempted_user)
+            flash(f'Success! You are logged in as: {attempted_user.username}', category='success')
+            return redirect(url_for('market_page'))
+        else:
+            flash('Username and password are not match! Please try again', category='danger')
+
     return render_template('login.html', form=form)
