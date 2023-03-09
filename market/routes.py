4c5edecda126202ef608ac74d3a2521d96a8785d
Javier Juan Uceda <javierjuanuceda18@gmail.com>
2023-03-09 09:43:07 +0100

    Commit 11 11 - User Authentication Part 1

--- market/routes.py
+++ market/routes.py
@@ -2,7 +2,7 @@
 from flask import render_template, redirect, url_for, flash
 
 from market import app, db
-from market.forms import RegisterForm
+from market.forms import RegisterForm, LoginForm
 from market.models import Item, User
 
 
@@ -31,3 +31,9 @@ def register_page():
             flash(f'There was an error with creating a user: {err_msg}', category='danger')
 
     return render_template('register.html', form=form)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login_page():
+    form = LoginForm()
+    return render_template('login.html', form=form)
